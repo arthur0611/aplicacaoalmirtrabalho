ff43b3279668a1b25c9a68abfdd4c47469245d68
arthur0611 <69803372+arthur0611@users.noreply.github.com>
2021-09-13 07:44:38 -0300

    termino de ,venda

--- Models/Dominio/venda.cs
+++ Models/Dominio/venda.cs
@@ -1,11 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace aplicacaoalmirtrabalho.Models.Dominio
 {
+    [Table("usuario")]
     public class venda
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DisplayName("ID")]
+        public int Id { get; set; }
+
+        //jogos
+
+        //usuario
+
+        [DisplayName("Preço total")]
+        [Required(ErrorMessage = "Campo preco total de jogo é obrigatório")]
+        public string precototal { get; set; }
+
+        [DisplayName("Preço total")]
+        [Required(ErrorMessage = "Campo como foi feita a compra de jogo é obrigatório")]
+        public string cffc { get; set; }
     }
 }
